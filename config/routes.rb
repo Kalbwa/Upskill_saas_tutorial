7f229bd9d17793088c384aa2ef954ba5ae2d03a2
EC2 Default User <bagayraz@protonmail.com>
2020-05-30 13:40:32 +0000

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
-  get 'contact-us', to: 'contacts#new'
+  
+  # Full routes access
+  # resources :contacts
+  
+  # Limited routes to two options only
+  # resources :contacts, only: [:new, :create]
+  
+  # Refactored to create action 
+  resources :contacts, only: :create
+  
+  # Refactored links to show to contact-us instead of contact/new
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
