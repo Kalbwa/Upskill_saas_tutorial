90600d2e0e0f0b8f8abc3697cb3fb5cef53ea4c9
EC2 Default User <bagayraz@protonmail.com>
2020-05-31 15:39:32 +0000

    Added the forgoten delivery_method to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Initialize the Rails application.
+Rails.application.initialize!
+
 ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
@@ -9,6 +12,4 @@ ActionMailer::Base.smtp_settings = {
   :domain         => 'fierce-plateau-01528.herokuapp.com',
   :authentication => :plain,
 }
-
-# Initialize the Rails application.
-Rails.application.initialize!
+ActionMailer::Base.delivery_method = :smtp
