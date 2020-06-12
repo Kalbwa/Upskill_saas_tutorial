1522948d46b4d79020c4ecca2710add82966792e
EC2 Default User <bagayraz@protonmail.com>
2020-06-12 16:44:43 +0000

    Added before filter to check current users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
   # Get to /users/:user_id/profile/new
   def new
     # Render blank profile details form
@@ -9,12 +12,12 @@ class ProfilesController < ApplicationController
   # POST to /users/:user_id/profile 
   def create
     # ensure that we have the user who is filling out form
-    @user = User.find(id: params[:user_id])
+    @user = User.find(params[:user_id])
     # Create profile linked to this specific user
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:success] = "Profile Updated"
-      redirect_to user_path(id: params[:user_id])
+      redirect_to user_path(params[:user_id])
     else
       render action: :new
     end
@@ -22,21 +25,21 @@ class ProfilesController < ApplicationController
   
   # GET to /users/:user_id/profile/edit
   def edit
-    @user = User.find(id: params[:user_id])
+    @user = User.find(params[:user_id])
     @profile = @user.profile
   end
   
   # PUT to /users/:user_id/profile
   def update
     # retrieeve user from the database
-    @user = User.find(id: params[:user_id])
+    @user = User.find(params[:user_id])
     # retrive that user profile
     @profile = @user.profile
     # Mass assign edited profile attributes and save (update)
     if @profile.update_attributes(profile_params)
       flash[:success] = "Profile Updated"
       # redirect to their profile page
-      redirect_to user_path(id: params[:user_id])
+      redirect_to user_path(params[:user_id])
     else
       render action: :edit
     end
@@ -45,5 +48,10 @@ class ProfilesController < ApplicationController
   private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+  end
+  
+  def only_current_user
+    @user = User.find(params[:user_id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
